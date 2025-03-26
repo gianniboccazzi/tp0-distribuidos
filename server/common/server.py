a8bf1fcb322a561ea003ac6b4cdb5bd4a2f771a9
Gianni Boccazzi <gianniboccazzi@gmail.com>
2025-03-26 18:23:31 -0300

    feat: add multiprocessing lib

--- server/common/server.py
+++ server/common/server.py
@@ -3,6 +3,7 @@ import socket
 import logging
 
 from communication.protocol import BetProtocol
+from multiprocessing import Process, Manager, Queue
 
 
 
@@ -16,14 +17,18 @@ class Server:
         self.lottery_completed = False
         self.protocol = BetProtocol()
         self.clients_total = clients_total
+        self.manager = Manager()
         self.clients_ready = set()
+        self.queue = Queue()
+        self.processes = []
 
     def run(self):
         signal.signal(signal.SIGTERM, self.__signal_handler)
         while self.active:
             client_sock = self.__accept_new_connection()
             if client_sock:
-                client_ready = self.protocol.handle_client_connection(client_sock, self.lottery_completed)
+                process = Process(target=self.protocol.handle_client_connection, args=(client_sock, self.lottery_completed))
+                process.start()
                 client_sock.close()
                 self.check_lottery_status(client_ready)
 
